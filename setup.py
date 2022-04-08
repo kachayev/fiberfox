1ebd51f1af7f8c54914c80678354e381d4b670de
Oleksii Kachaiev <kachayev@gmail.com>
2022-04-08 23:53:36 +0200

    0.3.2 -> 0.3.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf8") as f:
 
 setup(
     name="fiberfox",
-    version="0.3.2",
+    version="0.3.3",
     packages=find_packages(),
     data_files=[],
     entry_points={
